20e01f9e0afd660b89684fc97aa938533d61f8f8
Wongsatorn Pornpirunroch <wongsatorn.fw@gmail.com>
2024-04-08 14:07:51 +0700

    feat: simple homepage

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,26 +1,9 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-
-const socket = new WebSocket('ws://localhost:8080')
-
-socket.addEventListener('open', () => {
-  console.log('Connect to the server')
-})
-
-socket.addEventListener("message", (e) => {
-  console.log(e)
-})
-
-const message = ref("")
-
-function sendMessage() {
-  socket.send(message.value)
-}
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div class="flex h-screen items-center justify-center gap-x-4">
-    <input class="border" v-model="message" type="text">
-    <button class="px-4 py-2 bg-slate-200 border-2" @click="sendMessage()">Click to send message</button>
-  </div>
+  <main>
+    <RouterView></RouterView>
+  </main>
 </template>
---
+++ client/src/views/HomePage.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const name = ref('')
+const error = ref('')
+
+const router = useRouter()
+
+function joinChat() {
+  if (!name.value) {
+    error.value = 'Please enter your name before joining chat'
+    return
+  }
+  router.push({
+    name: 'ChatRoom',
+    state: {
+      name: name.value
+    }
+  })
+}
+</script>
+
+<template>
+  <div class="mx-auto flex h-screen max-w-7xl items-center justify-center">
+    <div class="w-full max-w-xl space-y-4 px-4 py-8 shadow-lg">
+      <h1 class="text-center text-xl font-bold">Simple Chat App</h1>
+      <div v-if="error" class="text-center text-red-500">** {{ error }} **</div>
+      <input
+        v-model="name"
+        class="block w-full border-2 px-4 py-2"
+        placeholder="Enter your name before joining chat"
+        type="text"
+      />
+      <button class="ml-auto block bg-gray-100 px-4 py-2 hover:bg-gray-300" @click="joinChat">
+        Join chat
+      </button>
+    </div>
+  </div>
+</template>
