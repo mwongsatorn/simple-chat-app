b18045f95b287c2e2b88cb5426b82c2b0de3702b
Wongsatorn Pornpirunroch <wongsatorn.fw@gmail.com>
2024-04-09 12:45:04 +0700

    fix(style): chatbox overflows with long text

--- client/src/components/Chatbox.vue
+++ client/src/components/Chatbox.vue
@@ -6,7 +6,7 @@ const props = defineProps<{
 </script>
 
 <template>
-  <div class="w-fit">
+  <div class="w-fit max-w-[50%]">
     <div class="font-bold">{{ props.sender }}</div>
     <div class="whitespace-pre-wrap break-words rounded-md bg-blue-500 px-4 py-2 text-white">
       {{ props.message }}
